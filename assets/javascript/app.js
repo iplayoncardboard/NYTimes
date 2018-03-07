7356aeb8776705a9127233b452c57c27622f916f
Erik C Hurst <ehurst01@gmail.com>
2018-03-06 21:15:04 -0700

    fixed Brit's stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,3 @@
-//Click event search buttton
-$('#runSearch').on("click", function(){
-    alert("search")
-});
 
 //click event reset button
 $('#clearAll').on('click',function(){
@@ -9,18 +5,18 @@ $('#clearAll').on('click',function(){
     alert("cleared")
 });
 
-
-$("submit").on("click", function() {
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=e597fe8f44504907a3558e207a65c21e" + search;
+//Click event search buttton
+$('#runSearch').on("click", function(event) {
+    event.preventDefault();
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=e597fe8f44504907a3558e207a65c21e&q=" + search;
     var search = $("#search").val();
-
 
   $.ajax({
         url: queryURL,
         method: "GET"
   })
   .then(function(response) {
-       
+       console.log(response);
   }
-  
+    )
   });
