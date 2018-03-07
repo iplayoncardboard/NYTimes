f2d4cf1a3c2dc4d331134154cdfe7ff0956569bb
unknown <brittanycturner@gmail.com>
2018-03-06 21:07:09 -0700

    AJAX

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,19 @@ $('#clearAll').on('click',function(){
 
     alert("cleared")
 });
+
+
+$("submit").on("click", function() {
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=e597fe8f44504907a3558e207a65c21e" + search;
+    var search = $("#search").val();
+
+
+  $.ajax({
+        url: queryURL,
+        method: "GET"
+  })
+  .then(function(response) {
+       
+  }
+  
+  });
