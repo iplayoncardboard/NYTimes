157ffb0b71c5c551a0868e8d866bec9007f4443b
Erik C Hurst <ehurst01@gmail.com>
2018-03-06 20:59:52 -0700

    linked JS to HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//Click event search buttton
+$('#runSearch').on("click", function(){
+    alert("search")
+});
+
+//click event reset button
+$('#clearAll').on('click',function(){
+
+    alert("cleared")
+});
